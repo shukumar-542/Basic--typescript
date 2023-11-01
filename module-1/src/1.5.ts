21932fa3b52ab56195789ffde74fd0c8d6ace938
shukumar-542 <shukumar542@gmail.com>
2023-11-01 22:26:40 +0600

    Referance type object

---
+++ module-1/src/1.5.ts
@@ -0,0 +1,12 @@
+// Reference Type Object
+
+
+const user : {
+    company : "coderex"; //literal type
+    firstName :  string;
+    lastName? : string;  //option type
+ } = {
+    company : "coderex",
+    firstName  : "shukumar",
+    // lastName : 'Ghosh',
+}
