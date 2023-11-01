2e8f2ccff8218cf0356a96d5e856e947a6f903b7
shukumar-542 <shukumar542@gmail.com>
2023-11-01 22:09:34 +0600

    Learn basic data types

---
+++ module-1/src/1.4.ts
@@ -0,0 +1,20 @@
+// Basics Data types
+
+// string
+let firstName : string = "shukumar";
+
+
+// number
+let age  : number = 12;
+
+// Null
+let y : null = null;
+
+
+// array
+let friends : string[] = ['abir', 'jafore'];
+
+// tupple --> array --> order --> types of values
+
+let  nameAge : [string, number] = ['shukumar', 28];
+ 
