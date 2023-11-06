374c2a3f8ec2d1ffbc00eea9df9f967f228b48e8
shukumar-542 <shukumar542@gmail.com>
2023-11-07 05:31:45 +0600

    polymorphism

---
+++ module-3/Polymorphism.ts
@@ -0,0 +1,29 @@
+{
+    // 
+
+    class Person {
+        getSleep(){
+            console.log('I am sleeping for 8 hours per day');
+        }
+    }
+    class Student extends Person {
+        getSleep()  {
+            console.log('Student Sleeping 10 hours per day');
+        }
+    }
+    class Developer extends Person {
+        getSleep()  {
+            console.log('Developer Sleeping 10 hours per day');
+        }
+    }
+   const getSleepHours = (param : Person)=>{
+    param.getSleep()
+   }
+    const student = new Student()
+    const developer =  new Developer()
+    getSleepHours(student)
+    getSleepHours(developer)
+
+
+    // 
+}
